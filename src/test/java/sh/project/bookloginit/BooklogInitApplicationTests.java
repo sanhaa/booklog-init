5f09f6cb0ef97a0837824e588a7f19a34cf4290b
Sanhaa <livia3609@gmail.com>
2023-11-27 23:24:31 +0900

    Initial commit

---
+++ src/test/java/sh/project/bookloginit/BooklogInitApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.project.bookloginit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklogInitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
